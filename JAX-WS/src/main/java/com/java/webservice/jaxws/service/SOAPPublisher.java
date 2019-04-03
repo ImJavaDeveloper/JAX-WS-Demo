c194f9582909f3d1b73ed5e2524547407f351abd
Rahat Ali <rahatiu786@gmail.com>
2019-04-03 23:00:53 +0530

    updated gradle to generate client

--- JAX-WS/src/main/java/com/java/webservice/jaxws/service/SOAPPublisher.java
+++ JAX-WS/src/main/java/com/java/webservice/jaxws/service/SOAPPublisher.java
@@ -1,11 +1,16 @@
 package com.java.webservice.jaxws.service;
 
+import java.net.BindException;
+
 import javax.xml.ws.Endpoint;
 
 public class SOAPPublisher {
 
-	public static void main(String[] args) {
-		 Endpoint.publish("http://localhost:8888/ws/person", new PersonServiceImpl());  
-	}
+	public static void main(String[] args) throws BindException {
+		 System.out.println("Publishing EndPoint ....");
+		Endpoint.publish("http://localhost:8881/ws/person", new PersonServiceImpl());	
+
+}
+
 
 }
